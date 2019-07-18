dc137d5957b3bef96ca9b0ad8d236618fe38a503
Jung-Woo-sik <o0o0@cs-cnu.org>
2019-07-18 18:10:31 +0900

    Question 41 python ing..

---
+++ Q41.py
@@ -0,0 +1,10 @@
+#Given an integer array, obtain the longest subarray of consecutive integers without duplicates.
+
+def solution(a):
+ 	b = list()
+ 	for i in a:
+		
+
+if __name__ == "__main__":
+    a = [2,0,2,1,4,3,1,0]
+	print(solution(a))
